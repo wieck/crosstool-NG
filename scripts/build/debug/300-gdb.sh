3ce0bf7b82b891db263f7e360289e5980fd4c901
Max Filippov <jcmvbkbc@gmail.com>
2014-11-15 14:04:31 +0300

    xtensa: fix gdb overlay application
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- scripts/build/debug/300-gdb.sh
+++ scripts/build/debug/300-gdb.sh
@@ -7,7 +7,7 @@ do_debug_gdb_get() {
 do_debug_gdb_extract() {
     CT_ExtractPatch GDB
 
-    if [ -n "${CT_ARCH_XTENSA_CUSTOM_OVERLAY_FILE}" ]; then
+    if [ -n "${CT_ARCH_XTENSA_CUSTOM_NAME}" ]; then
         CT_ConfigureXtensa "gdb" "${CT_GDB_VERSION}"
     fi
 }
